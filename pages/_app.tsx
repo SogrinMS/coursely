3bbcb22b66378c13e62b86360f6e727203dfaf18
Maxim <sogrinms@gmail.com>
2023-01-04 19:07:49 +0300

    Initial commit

---
+++ pages/_app.tsx
@@ -0,0 +1,9 @@
+import type { AppProps } from 'next/app';
+
+import '/src/styles/styles.scss';
+
+const App = ({ Component, pageProps }: AppProps) => {
+  return <Component {...pageProps} />;
+};
+
+export default App;
